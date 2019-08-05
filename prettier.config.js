931af82309b797cfda3fdb27e319fdb6a7c88a02
Jared Wuliger <jaredwuliger@gmail.com>
2019-08-04 22:23:37 -0400

    added prettier

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+// tslint:disable: object-literal-sort-keys
+
+module.exports = {
+	printWidth: 140,
+	tabWidth: 4,
+	trailingComma: 'none',
+	useTabs: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+	rangeStart: 0,
+	rangeEnd: Infinity,
+	requirePragma: false,
+	semi: true,
+	singleQuote: true,
+	insertPragma: false,
+	proseWrap: 'preserve'
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
