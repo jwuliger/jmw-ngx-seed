268d7c0dbff956ef8ee4e7853213fe33dfe7ad25
Jared Wuliger <jaredwuliger@gmail.com>
2019-08-12 00:20:56 -0400

    add documentation

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
 @NgModule({
-	imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+	imports: [
+		// The AppServerModule should import the AppModule followed
+		// by the ServerModule from @angular/platform-server.
+		AppModule,
+		ServerModule,
+		ModuleMapLoaderModule // <-- *Important* enables lazy-loaded routes to work
+	],
+	// Since the bootstrapped component is not inherited from the
+	// imported AppModule, it needs to be repeated here.
 	bootstrap: [AppComponent]
 })
 export class AppServerModule {}
